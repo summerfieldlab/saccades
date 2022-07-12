3e09ae1deedbf7d6a8427e0b699ec82f120000dd
Jessica Thompson <jessicathompson00@gmail.com>
2022-07-12 13:06:27 +0100

    add multiplicative model and alternative rnn implementation

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from scipy.stats import special_ortho_group
 
+
 class PixelPlusShapeModel(nn.Module):
     """RNN receives joint embedding of pixels and predicted shape label.
 
@@ -122,6 +123,7 @@ class ShapeModel(nn.Module):
         number = self.readout(number)
         return number, shape, hidden
 
+
 class Distinctive(nn.Module):
     def __init__(self, hidden_size, **kwargs):
         super().__init__()
@@ -162,6 +164,7 @@ class Distinctive(nn.Module):
         number = self.drop_layer(number)
         number = self.readout(number)
         return number, shape, hidden
+
 
 class DistinctiveCheat(nn.Module):
     def __init__(self, **kwargs):
@@ -266,6 +269,7 @@ class ContentGated_cheat(nn.Module):
 
         return min_shape, min_number, counts, maps, hidden
 
+
 # Models with integrated pixel and gaze streams
 class IntegratedModel(nn.Module):
     def __init__(self, input_size, hidden_size, map_size, output_size, **kwargs):
@@ -314,6 +318,7 @@ class IntegratedModel(nn.Module):
         number, map_, hidden = self.rnn(combined, hidden)
 
         return number, map_, hidden
+
 
 class TwoRNNs(nn.Module):
     def __init__(self, input_size, hidden_size, map_size, output_size, **kwargs):
@@ -371,7 +376,6 @@ class TwoRNNs(nn.Module):
         combined = torch.cat((pix, gaze), dim=1)
         number = self.fc(combined)
         return number, map_, hidden
-
 
 
 # Gaze only models
@@ -487,6 +491,7 @@ class ConvReadoutMapNet(nn.Module):
         number, _ = self.readout(map_to_pass_on)
         # number = torch.relu(self.fc(map))
         return number, map_, hidden
+
 
 class ConvNet(nn.Module):
     def __init__(self, width, height, output_size, dropout, big=False):
@@ -660,6 +665,7 @@ class TwoStepModel_weightshare_detached(TwoStepModel_weightshare):
         for param in self.rnn.parameters():
             param.requires_grad = False
 
+
 class Readout(nn.Module):
     def __init__(self, map_size, out_size, init):
         super(Readout, self).__init__()
@@ -700,6 +706,59 @@ class Readout(nn.Module):
         out = torch.addmm(self.bias, map, weight_mat)
         return out
 
+class RNN2(nn.Module):
+
+    # you can also accept arguments in your model constructor
+    def __init__(self, data_size, hidden_size, output_size, act=None):
+        super(RNN2, self).__init__()
+
+        self.hidden_size = hidden_size
+        input_size = data_size + hidden_size
+        if act == 'tanh':
+            self.act_fun = torch.tanh
+            gain = 5/3
+        elif act == 'relu':
+            self.act_fun = torch.relu
+            gain = np.sqrt(2)
+        elif act == 'sig':
+            self.act_fun = nn.Sigmoid()
+            gain = 1
+        elif act == 'lrelu':
+            self.act_fun = nn.LeakyReLU(0.1)
+            gain = np.sqrt(2)
+        else:
+            self.act_fun = None
+            gain = 1
+
+        self.i2h = nn.Linear(input_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.init_params(gain)
+
+
+    def forward(self, data, last_hidden):
+        input = torch.cat((data, last_hidden), 1)
+        hidden = self.i2h(input)
+        if self.act_fun is not None:
+            hidden = self.act_fun(hidden)
+        output = self.h2o(hidden)
+        return output, hidden
+
+    def init_params(self, gain):
+        if self.act_fun == 'relu':
+            nn.init.kaiming_uniform_(self.i2h.weight, a=math.sqrt(5), nonlinearity='relu')
+            nn.init.kaiming_uniform_(self.h2o.weight, a=math.sqrt(5), nonlinearity='relu')
+        elif self.act_fun == 'lrelu':
+            nn.init.kaiming_uniform_(self.i2h.weight, a=math.sqrt(5), nonlinearity='leaky_relu')
+            nn.init.kaiming_uniform_(self.h2o.weight, a=math.sqrt(5), nonlinearity='leaky_relu')
+        elif self.act_fun is None:
+            nn.init.kaiming_uniform_(self.i2h.weight, a=math.sqrt(5), nonlinearity='linear')
+            nn.init.kaiming_uniform_(self.h2o.weight, a=math.sqrt(5), nonlinearity='linear')
+        else:
+            nn.init.xavier_uniform_(self.i2h.weight, gain=gain)
+            nn.init.xavier_uniform_(self.h2o.weight, gain=gain)
+
+    def initHidden(self, batch_size):
+        return torch.zeros(batch_size, self.hidden_size)
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, act=None, eye_weight=False, dropout=0):
@@ -717,6 +776,9 @@ class RNN(nn.Module):
         elif act == 'sig':
             self.act_fun = nn.Sigmoid()
             gain = 1
+        elif act == 'lrelu':
+            self.act_fun = nn.LeakyReLU(0.1)
+            gain = np.sqrt(2)
         else:
             self.act_fun = None
             gain = 1
@@ -759,6 +821,102 @@ class RNN(nn.Module):
 
     def initHidden(self, batch_size):
         return torch.zeros(batch_size, self.hidden_size)
+
+
+class MultRNN(nn.Module):
+    """ Include multiplicative or gated interactions between input and hidden.
+
+    Normally this would require a weight tensor. Here we use a factorized
+    version as described in:
+    Sutskever, I., Martens, J., & Hinton, G. (2011). Generating text with
+    recurrent neural networks. Proceedings of the 28th International Conference
+    on Machine Learning, ICML 2011, 1017–1024.
+    """
+    def __init__(self, input_size, hidden_size, factor_size, output_size, small_weights):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.i2f = nn.Linear(input_size, factor_size, bias=False)
+        self.h2f = nn.Linear(hidden_size, factor_size, bias=False)
+        self.f2h = nn.Linear(factor_size, hidden_size, bias=False)
+        self.i2h = nn.Linear(input_size, hidden_size, bias=False)
+        self.h2o = nn.Linear(hidden_size, output_size, bias=True)
+        self.gain = 5/3
+        self.params = [self.i2f, self.h2f, self.f2h, self.i2h, self.h2o]
+        self.small_weights = small_weights
+        self.init_params()
+
+    def forward(self, x_t,  h_tm1):
+        # zTU = a.t @ self.i2ha.weight
+        # Vx = self.i2hb.weight @ b
+        # zTWx = a.t @ self.i2hab.weight @ b
+        # hidden = zTU + Vx + zTWx + self.i2h.bias
+        # output = self.h2o.weight
+        W_fx = self.i2f.weight
+        W_fh = self.h2f.weight
+        W_hf = self.f2h.weight
+        W_hx = self.i2h.weight
+        W_oh = self.h2o.weight
+        b_o = self.h2o.bias
+
+        # left = torch.diag(W_fx @ x_t.t())
+        left = torch.diag(x_t @ W_fx.t())
+        # right = W_fh @ h_tm1.t()
+        right = h_tm1 @ W_fh.t()
+        f_t = left * right
+        # f_t = left @ right
+        h_t = torch.tanh(f_t @ W_hf.t() + x_t @ W_hx.t())
+        o_t = h_t @ W_oh + b_o
+        return o_t, h_t
+
+    def init_params(self):
+        for par in self.params:
+            if self.small_weights:
+                nn.init.normal_(par.weight, mean=0, std=0.1)
+            else:
+                nn.init.xavier_uniform_(par.weight, gain=self.gain)
+
+    def initHidden(self, batch_size):
+        return torch.zeros(batch_size, self.hidden_size)
+
+
+class MultiplicativeLayer(nn.Module):
+    def __init__(self, z_size, x_size, out_size, small_weights):
+        super().__init__()
+        self.U = nn.Linear(z_size, out_size, bias=True)
+        self.V = nn.Linear(x_size, out_size, bias=False)
+        self.W = nn.Parameter(torch.zeros((z_size, out_size, x_size)))
+        self.params = [self.U.weight, self.V.weight, self.W]
+        self.small_weights = small_weights
+        self.init_params()
+
+    def init_params(self):
+        for par in self.params:
+            if self.small_weights:
+                nn.init.normal_(par, mean=0, std=0.1)
+            else:
+                nn.init.kaiming_uniform_(par, a=math.sqrt(5), nonlinearity='leaky_relu')
+
+
+    def forward(self, x, z):
+        # zTU = z.t @ self.U
+        # zTU = self.U(z)
+        # # Vx = self.V.weight @ x
+        # Vx = self.V(x)
+        # zTWx = z @ self.W @ x.t()
+        # # z.unsqueeze(1)
+        # # test = torch.tensordot(torch.tensordot(z, self.W), x.t())
+        # out = zTU + Vx + zTWx
+        zTW = torch.einsum('ij,jkl->ikl', z, self.W)
+        # zz = z.unsqueeze(2)
+        # torch.einsum('ijk,lk->ilj', self.W, z)
+        # np.einsum('ijk,lk->ilj', self.W, z)
+        # tensordot(a2D,a3D,((-1,),(-1,))).transpose(1,0,2)
+        W_prime =  zTW + self.V.weight
+        b_prime = self.U(z)
+
+        W_primex = torch.einsum('ij,ikj->ik', x, W_prime)
+        y = W_primex + b_prime
+        return y
 
 class linearRNN_hist(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
