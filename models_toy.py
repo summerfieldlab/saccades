426f49e5e417c920c314d006fc554fd15480b9c2
Jessica Thompson <jessicathompson00@gmail.com>
2022-11-22 14:05:18 +0000

    add gated models

--- models_toy.py
+++ models_toy.py
@@ -6,18 +6,204 @@ from scipy.stats import special_ortho_group
 from models import RNN, RNN2, MultRNN, MultiplicativeLayer
 
 
+class MapGated2RNN(nn.Module):
+    def __init__(self, sh_size, hidden_size, map_size, output_size, **kwargs):
+        super().__init__()
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        emb_size=100
+        # n_shapes = kwargs['n_shapes'] if 'n_shapes' in kwargs.keys() else 20
+        self.act = kwargs['act'] if 'act' in kwargs.keys() else None
+        self.detach = kwargs['detach'] if 'detach' in kwargs.keys() else False
+        drop = kwargs['dropout'] if 'dropout' in kwargs.keys() else 0
+        # self.par = kwargs['parallel'] if 'parallel' in kwargs.keys() else False
+        self.pix_embedding = nn.Linear(sh_size, hidden_size//2)
+        # self.shape_readout = nn.Linear(hidden_size//2, n_shapes)
+        self.xy_embedding = nn.Linear(2, hidden_size//2)
+        self.joint_embedding = nn.Linear(hidden_size, hidden_size)
+        self.rnn_map = RNN2(hidden_size, hidden_size, hidden_size, self.act)
+        self.rnn_gate = RNN2(hidden_size, hidden_size, hidden_size, self.act)
+        self.drop_layer = nn.Dropout(p=drop)
+
+        self.map_readout = nn.Linear(hidden_size, map_size)
+        self.gate = nn.Linear(hidden_size, map_size)
+        self.after_map = nn.Linear(map_size, map_size)
+        self.num_readout = nn.Linear(map_size, output_size)
+
+        self.sigmoid = nn.Sigmoid()
+        self.LReLU = nn.LeakyReLU(0.1)
+
+    def initHidden(self, batch_size):
+        hidden_m = self.rnn_map.initHidden(batch_size)
+        hidden_g = self.rnn_gate.initHidden(batch_size)
+        hidden = torch.cat((hidden_m, hidden_g), dim=1)
+        return hidden
+
+    def forward(self, x, hidden):
+        hidden_map = hidden[:, :self.hidden_size]
+        hidden_gate = hidden[:, self.hidden_size:]
+        xy = x[:,:2]  # xy coords are first two input features
+        shape = x[:,2:]
+        # gate = self.sigmoid(self.gate(shape))
+        # gated = torch.mul(x, gate)
+        # xy = gated[:,:2]  # xy coords are first two input features
+        # shape = gated[:,2:]
+        shape_emb = self.LReLU(self.pix_embedding(shape))
+        xy_emb = self.LReLU(self.xy_embedding(xy))
+        combined = torch.cat((shape_emb, xy_emb), dim=-1)
+        # gated_combined = torch.mul(combined, gate)
+        x = self.LReLU(self.joint_embedding(combined))
+        x_map, hidden_map = self.rnn_map(x, hidden_map)
+        x_gate, hidden_gate = self.rnn_gate(x, hidden_gate)
+        x_map = self.drop_layer(x_map)
+
+        full_map = self.map_readout(x_map)
+        # sig_full = self.sigmoid(full_map)
+        gate = self.sigmoid(self.gate(x_gate))
+        if self.detach:
+            # map_to_pass_on = sig_full.detach().clone()
+            map_to_pass_on = full_map.detach().clone()
+        else:
+            # map_to_pass_on = sig_full
+            map_to_pass_on = full_map
+        gated_map = torch.mul(map_to_pass_on, gate)
+        if self.detach:
+            gated_map_to_pass_on = gated_map.detach().clone()
+        else:
+            gated_map_to_pass_on = gated_map
+        penult = self.LReLU(self.after_map(gated_map_to_pass_on))
+        num = self.num_readout(penult)
+        hidden = torch.cat((hidden_map, hidden_gate), dim=1)
+        return num, shape, (full_map, gated_map), hidden
+
+
+class MapGatedSymbolicRNN(nn.Module):
+    def __init__(self, sh_size, hidden_size, map_size, output_size, **kwargs):
+        super().__init__()
+        self.output_size = output_size
+        emb_size=100
+        # n_shapes = kwargs['n_shapes'] if 'n_shapes' in kwargs.keys() else 20
+        self.act = kwargs['act'] if 'act' in kwargs.keys() else None
+        self.detach = kwargs['detach'] if 'detach' in kwargs.keys() else False
+        drop = kwargs['dropout'] if 'dropout' in kwargs.keys() else 0
+        # self.par = kwargs['parallel'] if 'parallel' in kwargs.keys() else False
+        self.pix_embedding = nn.Linear(sh_size, emb_size//2)
+        # self.shape_readout = nn.Linear(hidden_size//2, n_shapes)
+        self.xy_embedding = nn.Linear(2, emb_size//2)
+        self.joint_embedding = nn.Linear(emb_size, hidden_size)
+        self.rnn = RNN2(hidden_size, hidden_size, hidden_size, self.act)
+        self.drop_layer = nn.Dropout(p=drop)
+
+        self.map_readout = nn.Linear(hidden_size, map_size)
+        self.gate = nn.Linear(hidden_size, map_size)
+        self.after_map = nn.Linear(map_size, map_size)
+        # if self.par:
+        #     self.notmap = nn.Linear(hidden_size, map_size)
+        #     self.num_readout = nn.Linear(map_size * 2, output_size)
+        # else:
+        self.num_readout = nn.Linear(map_size, output_size)
+
+        self.initHidden = self.rnn.initHidden
+        self.sigmoid = nn.Sigmoid()
+        self.LReLU = nn.LeakyReLU(0.1)
+
+    def forward(self, x, hidden):
+        xy = x[:,:2]  # xy coords are first two input features
+        shape = x[:,2:]
+        # gate = self.sigmoid(self.gate(shape))
+        # gated = torch.mul(x, gate)
+        # xy = gated[:,:2]  # xy coords are first two input features
+        # shape = gated[:,2:]
+        shape_emb = self.LReLU(self.pix_embedding(shape))
+        xy_emb = self.LReLU(self.xy_embedding(xy))
+        combined = torch.cat((shape_emb, xy_emb), dim=-1)
+        # gated_combined = torch.mul(combined, gate)
+        x = self.LReLU(self.joint_embedding(combined))
+        x, hidden = self.rnn(x, hidden)
+        x = self.drop_layer(x)
+
+        full_map = self.map_readout(x)
+        # sig_full = self.sigmoid(full_map)
+        gate = self.sigmoid(self.gate(x))
+        if self.detach:
+            # map_to_pass_on = sig_full.detach().clone()
+            map_to_pass_on = full_map.detach().clone()
+        else:
+            # map_to_pass_on = sig_full
+            map_to_pass_on = full_map
+        gated_map = torch.mul(map_to_pass_on, gate)
+        penult = self.LReLU(self.after_map(gated_map))
+        num = self.num_readout(penult)
+        return num, shape, (full_map, gated_map), hidden
+
+
+class GatedSymbolicRNN(nn.Module):
+    def __init__(self, sh_size, hidden_size, map_size, output_size, **kwargs):
+        super().__init__()
+        self.output_size = output_size
+        # n_shapes = kwargs['n_shapes'] if 'n_shapes' in kwargs.keys() else 20
+        self.act = kwargs['act'] if 'act' in kwargs.keys() else None
+        self.detach = kwargs['detach'] if 'detach' in kwargs.keys() else False
+        drop = kwargs['dropout'] if 'dropout' in kwargs.keys() else 0
+        # self.par = kwargs['parallel'] if 'parallel' in kwargs.keys() else False
+        self.pix_embedding = nn.Linear(sh_size, hidden_size//2)
+        # self.shape_readout = nn.Linear(hidden_size//2, n_shapes)
+        self.xy_embedding = nn.Linear(2, hidden_size//2)
+        self.gate = nn.Linear(sh_size, sh_size+2)
+        self.joint_embedding = nn.Linear(hidden_size, hidden_size)
+        self.rnn = RNN2(hidden_size, hidden_size, hidden_size, self.act)
+        self.drop_layer = nn.Dropout(p=drop)
+        self.map_readout = nn.Linear(hidden_size, map_size)
+        self.after_map = nn.Linear(map_size, map_size)
+        # if self.par:
+        #     self.notmap = nn.Linear(hidden_size, map_size)
+        #     self.num_readout = nn.Linear(map_size * 2, output_size)
+        # else:
+        self.num_readout = nn.Linear(map_size, output_size)
+
+        self.initHidden = self.rnn.initHidden
+        self.sigmoid = nn.Sigmoid()
+        self.LReLU = nn.LeakyReLU(0.1)
+
+    def forward(self, x, hidden):
+        xy = x[:,:2]  # xy coords are first two input features
+        shape = x[:,2:]
+        gate = self.sigmoid(self.gate(shape))
+        gated = torch.mul(x, gate)
+        xy = gated[:,:2]  # xy coords are first two input features
+        shape = gated[:,2:]
+        shape_emb = self.LReLU(self.pix_embedding(shape))
+        xy_emb = self.LReLU(self.xy_embedding(xy))
+        combined = torch.cat((shape_emb, xy_emb), dim=-1)
+        # gated_combined = torch.mul(combined, gate)
+        x = self.LReLU(self.joint_embedding(combined))
+        x, hidden = self.rnn(x, hidden)
+        x = self.drop_layer(x)
+
+        map_ = self.map_readout(x)
+        sig = self.sigmoid(map_)
+        if self.detach:
+            map_to_pass_on = sig.detach().clone()
+        else:
+            map_to_pass_on = sig
+        penult = self.LReLU(self.after_map(map_to_pass_on))
+        num = self.num_readout(penult)
+        return num, shape, map_, hidden
+
 class RNNClassifier2stream(nn.Module):
     def __init__(self, pix_size, hidden_size, map_size, output_size, **kwargs):
         super().__init__()
         # map_size = 9
         self.output_size = output_size
+        n_shapes = kwargs['n_shapes'] if 'n_shapes' in kwargs.keys() else 20
         self.act = kwargs['act'] if 'act' in kwargs.keys() else None
         self.detach = kwargs['detach'] if 'detach' in kwargs.keys() else False
         drop = kwargs['dropout'] if 'dropout' in kwargs.keys() else 0
         self.par = kwargs['parallel'] if 'parallel' in kwargs.keys() else False
         self.pix_embedding = nn.Linear(pix_size, hidden_size//2)
+        self.shape_readout = nn.Linear(hidden_size//2, n_shapes)
         self.xy_embedding = nn.Linear(2, hidden_size//2)
-        self.joint_embedding = nn.Linear(hidden_size, hidden_size)
+        self.joint_embedding = nn.Linear(hidden_size + n_shapes, hidden_size)
         self.rnn = RNN2(hidden_size, hidden_size, hidden_size, self.act)
         self.drop_layer = nn.Dropout(p=drop)
         self.map_readout = nn.Linear(hidden_size, map_size)
@@ -37,7 +223,8 @@ class RNNClassifier2stream(nn.Module):
         pix = x[:,2:]
         xy = self.LReLU(self.xy_embedding(xy))
         pix = self.LReLU(self.pix_embedding(pix))
-        combined = torch.cat((xy, pix), dim=-1)
+        shape = self.shape_readout(pix)
+        combined = torch.cat((shape, xy, pix), dim=-1)
         x = self.LReLU(self.joint_embedding(combined))
         x, hidden = self.rnn(x, hidden)
         x = self.drop_layer(x)
@@ -56,7 +243,7 @@ class RNNClassifier2stream(nn.Module):
             penult = torch.cat((penult, notmap), dim=1)
         # num = self.num_readout(map_to_pass_on)
         num = self.num_readout(penult)
-        return num, map_, hidden
+        return num, shape, map_, hidden
 
 
 class RNNClassifier(nn.Module):
