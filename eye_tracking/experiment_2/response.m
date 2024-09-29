3fcd30c72953621ed4a525cbc68aafcf617c651a
Julian Sandbrink <131259233+sandjuli@users.noreply.github.com>
2024-09-29 15:43:08 -0400

    Fixed formatting and updated comments in response.m

--- eye_tracking/experiment_2/response.m
+++ eye_tracking/experiment_2/response.m
@@ -1,15 +1,9 @@
-% function [resp_key,t1,earlyResponse] = response(window,t0,eyetracking,trial,MaxTime)
 function [key_pressed,t1, t2, fixation, first_time, first_time_poss_wrong] = response(window,t0,t1,eyetracking,trial, el, first_time, ImSq, fixation_condition, block,fixation)
 
 textcolor = [0 0 0];
-
 confirmcolor = [0.3 0.3 0.3];
 numchoices = 4;
 numcolor = 1; 
-
-% fixation = 0
-
-% fixation_condition = 1;
 
 spaceKeyCode = KbName('space');
 
@@ -26,6 +20,7 @@ buttonmargin=height/6;
 buttonheight = height-(5*buttonmargin); % 1/4 of screen
 buttonwidth = width/(2*(numchoices+ceil(numchoices/2)));
 % num of buttons + half the width of each button as spacer
+
         
 %% SET UP BUTTONS
 spacerwidth=buttonwidth/2;
@@ -33,18 +28,18 @@ spacerwidth=buttonwidth/2;
 buttonbottom=(height+buttonheight)/2;
 buttontop=(height-buttonheight)/2;
       
-   % initialize button matrix and assign coordinates of 1st button
+% Initialize button matrix and assign coordinates of 1st button
 buttons=repmat([width/2-2.75*buttonwidth    buttontop  ...
                 width/2-1.75*buttonwidth    buttonbottom], numchoices, 1);
-   % adjust X coordinates (Y coordinates are constant)
+% Adjust X coordinates (Y coordinates are constant)
 for i=2:4
     buttons(i,[1 3]) = buttons(i-1,[1 3]) + buttonwidth + spacerwidth; % advance horizontally
 end
 
-%% GET USER RESPONSE (new)
+
+%% GET USER RESPONSE
 earlyResponse = false;
 respToBeMade = true;
-% response_start_time = GetSecs
 
 if eyetracking
     Eyelink('message',['start accept reaction response ' num2str(trial) ' Block ' num2str(block)]);
@@ -59,41 +54,8 @@ if first_time
             earlyResponse = true;
             respToBeMade = false;
             int_t = GetSecs - t0;
-    %     if keyIsDown % printing out key presses to help debug
-    %         disp('key press detected')
-    %         disp('keyCode:')
-    %         disp(find(keyCode))
-    %     end
-    %    if keyIsDown
-    %     if keyCode(10)
-    %         ShowCursor;
-    %         sca;
-    %         return
-    %     elseif keyCode(12)
-    %         resp_key = 2;
-    %         respToBeMade = false;
-    %         earlyResponse = true;
-    %     elseif keyCode(13)
-    %         resp_key = 3;
-    %         respToBeMade = false;
-    %         earlyResponse = true;
-    %     elseif keyCode(14)
-    %         resp_key = 4;
-    %         respToBeMade = false;
-    %         earlyResponse = true;
-    %     elseif keyCode(15)
-    %         resp_key = 5;
-    %         respToBeMade = false;
-    %         earlyResponse = true;
-    %     elseif keyCode(16)
-    %         resp_key = 6;
-    %         respToBeMade = false;
-    %         earlyResponse = true;
-    %     elseif keyCode(17)
-    %         resp_key = 7;
-    %         respToBeMade = false;
-    %         earlyResponse = true;
-        % check if 2 seconds have passed since the stimulus was presented
+            
+        % Check if 2 seconds have passed since the stimulus was presented
         elseif GetSecs - t0 > 2
             respToBeMade = false;
         end
@@ -108,8 +70,6 @@ if first_time
             fixation(j) = check_fixation(el,scH,scW,siz); % double check what fixation does and incorporate the arguments accordingly
         end
 
-       % disp('While loops running')
-
     end
     
     if earlyResponse
@@ -122,56 +82,6 @@ if first_time
     end  
 
 end
-    % if there was an early response, move to the confirmation screen
-    % otherwise, continue with the regular response collection
-% disp('while loop done')
-
-
-
-%     % DRAW THE BUTTONS    
-%     Screen(window,'TextFont','Arial');
-%     Screen(window,'TextSize',80);   
-%     if numcolor ~= -1
-%       for i=2:numchoices+1
-%         numX = buttons(i-1,1) + (buttonwidth/3);
-%         numY = height/2;
-%         Screen('DrawText',window, num2str(i), numX, numY, numcolor);
-%       end
-%     end
-% 
-%     % Confirmation Screen
-%     Screen(window,'TextSize',40); 
-%     conf_instr = 'confirm your answer by pressing your response again';
-%     conf_instr_2 = 'press any other keys to re-select your response';
-%     Screen('DrawText',window, conf_instr, 200, buttonmargin+100, textcolor);  
-%     Screen('DrawText',window, conf_instr_2, 250, buttonmargin+175, textcolor);  
-% 
-%     for i = 1:6
-%         if i == (resp_key-1)
-%             Screen('FillRect',window,textcolor, buttons(i,:));
-%         else
-%             Screen('FillRect',window,confirmcolor, buttons(i,:));
-%         end
-% 
-%     end
-% 
-%     % key_pressed = resp_key;
-%     % return;
-% 
-%     Screen(window,'TextSize',80);   
-%     for i=2:numchoices+1
-%         numX = buttons(i-1,1) + (buttonwidth/3);
-%         numY = height/2;
-%         Screen('DrawText',window, num2str(i), numX, numY, numcolor);
-%       end
-%     Screen('Flip',window);
-%     
-%     key_pressed = resp_key;
-%     return;
-%     
-    
-
-%t1 = GetSecs - t0;
 
 
 if first_time
@@ -212,11 +122,6 @@ Screen(window,'TextSize',40);
 Screen('DrawText',window, 'Select your response by pressing the corresponding', 220, buttonmargin+100, textcolor);  
 Screen('DrawText',window, 'number on the keyboard', 400, buttonmargin+175, textcolor);  
 
-%Screen('Flip',window,0,1);
-%   (moved this to the run_experiment file to avoid multiple triggers)
-%     if eyetracking
-%         Eyelink('message',['stimulus offset ' num2str(trial)])
-%     end
 Screen('FillRect',window,textcolor, buttons');
 
 
@@ -224,7 +129,6 @@ Screen('FillRect',window,textcolor, buttons');
 Screen(window,'TextFont','Arial');
 Screen(window,'TextSize',80);   
 if numcolor ~= -1
-  % for i=3:numchoices+2
   for i=1:4     
     numX = buttons(i,1) + (buttonwidth/3);
     numY = height/2;
@@ -239,8 +143,6 @@ if eyetracking
 end
 
 
-
-
 % While loop checking for response
 respToBeMade = true;
 while respToBeMade == true
@@ -249,9 +151,6 @@ while respToBeMade == true
         ShowCursor;
         sca;
         return
-%     elseif keyCode(12)
-%         resp_key = 2;
-%         respToBeMade = false;
     elseif keyCode(13)
         resp_key = 3;
         respToBeMade = false;
@@ -264,15 +163,12 @@ while respToBeMade == true
     elseif keyCode(16)
         resp_key = 6;
         respToBeMade = false;
-%     elseif keyCode(17)
-%         resp_key = 7;
-%         respToBeMade = false;
     end
 end
 
 t2 = GetSecs - t0;
 
-% confirmation screen follows initial choice
+% CONFIRMATION SCREEN FOLLOWING INITIAL CHOICE
 Screen(window,'TextSize',40); 
 conf_instr = 'confirm your answer by pressing your response again';
 conf_instr_2 = 'press any other keys to re-select your response';
@@ -285,11 +181,9 @@ for i = 1:4
     else
         Screen('FillRect',window,confirmcolor, buttons(i,:));
     end
-
 end
 
 Screen(window,'TextSize',80);   
-% for i=2:numchoices+1
 for i=1:4
     numX = buttons(i,1) + (buttonwidth/3);
     numY = height/2;
@@ -306,9 +200,7 @@ end
 first_time = false;
 
 
-
 key_pressed = resp_key;
-% t1 = GetSecs - t0;
 if eyetracking
     Eyelink('message',['first response registered ' num2str(trial) ' Block ' num2str(block)]);
 end
